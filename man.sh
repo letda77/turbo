ed3c7b4fb96424632a7b52384b04276487590cb7
letda77 <92427749+letda77@users.noreply.github.com>
2022-01-14 01:39:27 +0700

    Delete man.sh

--- man.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=https://server1.whalestonpool.com
-WALLET=EQDxb4P9Rn74K6YufjXKulboKUys5Xd4lHJjv7D_OPc6OUGg
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-
-cd "$(dirname "$0")"
-
-chmod +x ./turbo && sudo ./turbo -a ethash -o $POOL -u $WALLET.$WORKER $@
